6ee2031ea2ae2e8bed60d452d40bc0bf2944e418
root <root@escaux>
2015-03-16 17:11:17 +0100

    cleanup src tree, added start/stop scripts

---
+++ src/main/bin/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp ORlib/OpenRate-1.5.2.3.jar:lib/*:properties:dist/ORFuzer-0.0.0.jar OpenRate.OpenRate -p Fuzer.properties.xml &
---
+++ src/main/bin/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Framework:Shutdown=true" >Semaphore.txt
+
